1f64854588339c7bf96433f316ccba1f9c88499f
Katya Belova <katrin.belova@gmail.com>
2018-03-28 16:23:50 +0300

    Sticky nav and popup add/remove.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,40 @@
 const $ = window.$ = window.jQuery = require('jquery');
 
+// Sticky nav
+$(window).scroll(function(){
 
+let fromTop = $(this).scrollTop();
+
+if(fromTop > 130) {
+
+		$('.mainHeader').addClass('header-top');
+	} else {
+
+		$('.mainHeader').removeClass('header-top');
+	}
+
+	if(fromTop > 140) {
+
+		$('.mainHeader').addClass('fixed-header');
+	} else {
+
+		$('.mainHeader').removeClass('fixed-header');
+	}
+});
+
+// Popup window
+$(function () {
+
+	$('.plan__button').on('click', function () {
+		$('#popup').removeClass('display_hidden').addClass('display_visible');
+	});
+
+	$('.popup__exit').on('click', function () {
+		$('#popup').removeClass('display_visible').addClass('display_hidden');
+	});
+
+	$('.popup_background').on('click', function(){
+		$('#popup').removeClass('display_visible').addClass('display_hidden');
+	});
+
+});
