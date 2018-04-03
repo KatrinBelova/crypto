e61b78a3068f76fe7264e4ce91ea7454d526aaf6
Katya Belova <katrin.belova@gmail.com>
2018-04-03 18:39:27 +0300

    Added Animation.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const $ = window.$ = window.jQuery = require('jquery');
 
-import './assets/scripts/jquery.waypoints.min.js';
-// import './assets/scripts/animate-css';
-import './assets/scripts/animations';
+import AOS from 'aos';
+
+AOS.init({
+  disable: 'mobile'
+});
+
 import './components/main-header/main-header';
 import './components/popup/popup';
 import './components/faq/faq';
+
+
