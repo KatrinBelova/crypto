2f9a6f6c0634abb1d019138696d2a4ca70557cc2
Katya Belova <katrin.belova@gmail.com>
2018-04-04 17:58:16 +0300

    New modal.

--- src/components/popup/popup.js
+++ src/components/popup/popup.js
@@ -15,26 +15,37 @@ import $ from 'jquery';
 //
 // // Popup change <h2> membership
 // $('#plan__button_monthly').on('click', function (e) {
-// 		$('.popup__membership').empty();
-// 		$('.popup__membership').text( 'monthly membership' );
+//    $('.popup__membership').empty();
+//    $('.popup__membership').text( 'monthly membership' );
 // });
 //
 // $('#plan__button_yearly').on('click', function (e) {
-// 		$('.popup__membership').empty();
-// 		$('.popup__membership').text( 'annual membership' );
+//    $('.popup__membership').empty();
+//    $('.popup__membership').text( 'annual membership' );
 // });
 //
 // $('#plan__button_lifetime').on('click', function (e) {
-// 		$('.popup__membership').empty();
-// 		$('.popup__membership').text( 'lifetime membership' );
+//    $('.popup__membership').empty();
+//    $('.popup__membership').text( 'lifetime membership' );
 // });
+
+
+
+
 
 import vex from 'vex-js';
 // vex.registerPlugin(require('vex-dialog'));
 // vex.defaultOptions.className = 'vex-theme-os';
 
-$(document).on('click', '[data-vex-toggle]', function (e) {
-  e.preventDefault();
+$(document).on('click', '#plan__button_monthly', function (e) {
+ 
+
+   // $('.modal__membership').empty();
+  
+   // $('.modal__membership').prepend( 'monthly membership' );
+ 
+
+ e.preventDefault();
 
   const $trigger = $(this);
   const $template = $(`[data-vex-modal="${$trigger.data('vex-toggle')}"]`);
@@ -44,8 +55,12 @@ $(document).on('click', '[data-vex-toggle]', function (e) {
   }
 
   vex.open({
-    unsafeContent: $template.html(),
+    unsafeContent: $template.html(
+      ),
     className: 'vex-theme-default',
-    closeClassName: 'modal__close'
+    closeClassName: 'modal__close',
+
   });
+
 });
+
